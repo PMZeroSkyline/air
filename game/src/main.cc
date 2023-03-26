d607d2d6ab388ed8b7128a6938f15133bf13c491
pmzeroskyline <guzhang0@gmail.com>
2023-03-26 18:34:42 +0800

    res

--- game/src/main.cc
+++ game/src/main.cc
@@ -5,7 +5,17 @@
 
 int main()
 {
+	CDAppleResourcesDir();
 
+ 	// read file via ASSIMP
+    Assimp::Importer importer;
+    const aiScene* scene = importer.ReadFile("../game/res/sushi_bar/scene.gltf", aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
+    // check for errors
+    if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // if is Not Zero
+    {
+        cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << endl;
+        return 0;
+    }
 	vec3 e(0.f ,90.f, 0.f);
 	quat q = EulerToQuat(radians(e));
 	e = degrees(eulerAngles(q));
@@ -14,7 +24,6 @@ int main()
 
 	return 0;
 
-	CDAppleResourcesDir();
 
 	Window window;
 	
