a196a78538bd6ea76cd535d65086cc49984341d9
pmzeroskyline <guzhang0@gmail.com>
2023-03-26 18:48:53 +0800

    file system

--- game/src/main.cc
+++ game/src/main.cc
@@ -2,14 +2,18 @@
 #include "Window.h"
 #include "Image.h"
 #include "GL.h"
+#include <iostream>
+#include <filesystem>
+namespace fs = std::filesystem;
 
 int main()
 {
 	CDAppleResourcesDir();
-
+	fs::current_path("../res");
+    std::cout << "Current working directory: " << fs::current_path() << '\n'; 
  	// read file via ASSIMP
     Assimp::Importer importer;
-    const aiScene* scene = importer.ReadFile("../game/res/sushi_bar/scene.gltf", aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
+    const aiScene* scene = importer.ReadFile("sushi_bar/scene.gltf", aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
     // check for errors
     if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // if is Not Zero
     {
@@ -22,14 +26,17 @@ int main()
 	cout << e << endl;
 	// Result : 0,89.9802,0,
 
-	return 0;
-
-
 	Window window;
 	
 	while (window.IsOpen())
 	{
 		window.Tick();
+
+		if (window.keys[GLFW_KEY_ESCAPE].pressDown)
+		{
+			window.Close();
+		}
+		
 	}
 
 	return 0;
